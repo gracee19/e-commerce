00b4107e099a68c3596b520947cefe0861f44753
Grace Palenzuela <gracee_palenzuela@yahoo.com>
2021-04-30 16:15:00 +0930

    sequelize connection in server.js added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,15 +1,14 @@
 const router = require('express').Router();
-const { canTreatArrayAsAnd } = require('sequelize/types/lib/utils');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   try{
     const categoryData = await Category.findAll({
       include: [{model: Product}]
-    })
+    });
     res.status(200).json(categoryData);
   }catch (err) {
     res.status(500).json(err);
@@ -17,23 +16,37 @@ router.get('/', (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   try{
-
-  }catch{}
+    const categoryData = await Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [{model:Product}]
+    });
+    res.status(200).json(categoryData);
+  }catch (err) {
+    res.status(500).json(err);
+  };
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 
