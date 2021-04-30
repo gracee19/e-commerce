cdec712ad0eb24949651c764f49732781c205727
Grace Palenzuela <gracee_palenzuela@yahoo.com>
2021-04-30 13:56:51 +0930

    Model Updated

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,15 +1,27 @@
 const router = require('express').Router();
+const { canTreatArrayAsAnd } = require('sequelize/types/lib/utils');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
+  try{
+    const categoryData = await Category.findAll({
+      include: [{model: Product}]
+    })
+    res.status(200).json(categoryData);
+  }catch (err) {
+    res.status(500).json(err);
+  };
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  try{
+
+  }catch{}
   // be sure to include its associated Products
 });
 
