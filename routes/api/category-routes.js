9a41c3102400766fc4baea14a6b095f588c98016
Grace Palenzuela <gracee_palenzuela@yahoo.com>
2021-04-30 19:59:40 +0930

    product and tag routes added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,8 +3,8 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// find all categories including associated Products
 router.get('/', async (req, res) => {
-  // find all categories
   try{
     const categoryData = await Category.findAll({
       include: [{model: Product}]
@@ -13,11 +13,10 @@ router.get('/', async (req, res) => {
   }catch (err) {
     res.status(500).json(err);
   };
-  // be sure to include its associated Products
 });
 
+// find one category by its `id` including its associated Products
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
   try{
     const categoryData = await Category.findOne({
       where: {
@@ -29,11 +28,11 @@ router.get('/:id', async (req, res) => {
   }catch (err) {
     res.status(500).json(err);
   };
-  // be sure to include its associated Products
+  
 });
 
+// create a new category
 router.post('/', async (req, res) => {
-  // create a new category
   try{
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
@@ -42,12 +41,37 @@ router.post('/', async (req, res) => {
   }
 });
 
+// update a category by its `id` value
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body,{
+      where: {id: req.params.id}
+    });
+    res.status(200).json(categoryData);
+  }catch (err) {
+    res.status(400).json(err);
+  }
 });
 
+// delete a category by its `id` value
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
+  
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
